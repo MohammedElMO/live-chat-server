1227d4f73acd2d1270dc0fd87a5d5c7c56728c11
MohammedElMO <amranimohamad040@gmail.com>
2024-10-23 18:46:19 +0100

    working on forwarding messages to the user

--- socker.ts
+++ socker.ts
@@ -5,22 +5,32 @@ import cors from "cors";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 
 const server = createServer(app);
 
-const io = new Server(server);
-
+const io = new Server(server, {
+  cors: {
+    credentials: false,
+    origin: "*",
+  },
+});
 
 io.on("connection", (socket) => {
+  socket.on("sent message", (message) => {
+	console.log("user sent this message : ",message)
+    socket.emit("foward message", message);
+  });
+
   socket.on("disconnect", () => {
     console.log("bye");
   });
   console.log("we have new user");
 });
 
-server.listen(2024, () => console.log("serving messges..."));
+server.listen(4000, () => console.log("serving messges..."));
+	
