cad0707d73a937562e093ae6ad5209c3718431fa
MohammedElMO <amranimohamad040@gmail.com>
2024-10-24 19:59:52 +0100

    feat/ showing the availabale chat for the current connected socket in the specific room - using fs as a storage

--- socker.ts
+++ socker.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
-import cors from "cors";
-
+import { readFile, writeFile } from "node:fs/promises";
 const app = express();
 
 // app.use(
@@ -20,10 +19,84 @@ const io = new Server(server, {
   },
 });
 
+type RoomT =
+  | {
+      roomName: { message: string }[];
+    }
+  | null
+  | undefined;
+
+
+async function writeMessagesToRoom(msg: string, room: string, roomData: RoomT) {
+  const options = {
+    flag: "w",
+  };
+
+  if (!roomData || !Object.keys(roomData).length) {
+    await writeFile(
+      "messages.json",
+      JSON.stringify({
+        [room]: [{ message: msg }],
+      }),
+      options
+    );
+  } else {
+    const registredMessages = roomData[room];
+
+    if (!registredMessages || !Object.keys(registredMessages).length) {
+      await writeFile(
+        "messages.json",
+        JSON.stringify({
+          ...roomData,
+
+          [room]: [{ message: msg }],
+        }),
+        options
+      );
+    } else {
+      const data = JSON.stringify({
+        ...roomData,
+        [room]: [...registredMessages, { message: msg }],
+      });
+      await writeFile("messages.json", data, options);
+    }
+  }
+}
+async function readAllMessages() {
+  try {
+    const file = await readFile("messages.json", { flag: "r" });
+    const content = JSON.parse(file.toString());
+    console.log("coontent ", content);
+    return content;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 io.on("connection", (socket) => {
-  socket.on("sent message", (message) => {
-	console.log("user sent this message : ",message)
-    socket.emit("foward message", message);
+  socket.on("join room", async (room) => {
+    const data: RoomT = await readAllMessages();
+    socket.join(room);
+    if (data && !Object.keys(data).length) {
+      socket.emit("join", []);
+    } else {
+      if (data) {
+        socket.emit("join", data[room]);
+      }
+    }
+  });
+
+  socket.on("sent message", async (message, roomName) => {
+    try {
+      const file = await readFile("messages.json", { flag: "r" });
+      const content = JSON.parse(file.toString());
+      console.log("coontent ", content);
+      await writeMessagesToRoom(message, roomName, content);
+    } catch (error) {
+      console.log(error);
+    }
+
+    io.to(roomName).emit("foward message", message);
   });
 
   socket.on("disconnect", () => {
@@ -33,4 +106,3 @@ io.on("connection", (socket) => {
 });
 
 server.listen(4000, () => console.log("serving messges..."));
-	
